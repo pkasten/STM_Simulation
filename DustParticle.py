cd03c530a1c855d3a1ec51f3e06a63835ea23cef
Johannes Tim Seifert <johannes.seifert@tu-braunschweig.de>
2021-06-23 19:28:11 +0200

    Add comments DustParticle

--- DustParticle.py
+++ DustParticle.py
@@ -8,8 +8,21 @@ import Configuration as cfg
 import numpy as np
 
 class DustParticle(Particle):
+    """
+    Dust Particle extends a Particle and describes Dust-like particles that can be added to the Dataframe
+    """
 
     def __init__(self, pos=None, color=None, size=30, maxArm=30, div=2.4):
+        """
+        Initializes a new dust particle
+        :param pos: position where particle is positioned, random if None
+        :param color: color of each circle, can be nagative. Random if None
+        :param size: approximate size of the Dust particle
+        :param maxArm: maximum arm length, corresponds to e size
+        :param div: scaling factor
+        """
+
+        # Set position
         if pos is None:
             x = Distance(False, random.randint(0 - cfg.get_px_overlap(), cfg.get_width().px + cfg.get_px_overlap()))
             y = Distance(False, random.randint(0 - cfg.get_px_overlap(), cfg.get_height().px + cfg.get_px_overlap()))
@@ -17,13 +30,16 @@ class DustParticle(Particle):
         else:
             self.pos = pos
 
+        # Set color
         if color is None:
             self.color = random.randint(-25, 50)
         else:
             self.color = color
 
+        # Initialize super
         super().__init__(self.pos[0], self.pos[1], 0)
 
+        # get image properties
         self.img_w = cfg.get_width()
         self.img_h = cfg.get_height()
 
@@ -31,11 +47,14 @@ class DustParticle(Particle):
         #if size is None:
         #    self.size = random.random() * 40
         #else:
+
+        # Set size to random value
         self.size = size * random.random() / div
 
-
+        # Number of arms for fractal root
         self.arms = random.randint(1,5)
 
+        # set minimum and maximum values for circle radius and arm length
         self.circle_min = 1
 
         if maxArm is None:
@@ -46,16 +65,13 @@ class DustParticle(Particle):
         self.arm_min = self.circle_min
         self.arm_max = self.cirlce_max
 
-
-
-
-
+        # Initialize arrays for circle positons etc
         self.relpos = []
         knots = []
         self.circles = []
 
 
-
+        # Add first layer of fractal circles
         center = np.array([0, 0])
         for i in range(self.arms):
             a = self.angle()
@@ -63,6 +79,7 @@ class DustParticle(Particle):
             knots.append(center + vers)
             self.relpos.append(center+vers)
 
+        # Add more layers
         for knot in knots:
             if(random.random() < self.poss()):
                 a = self.angle()
@@ -71,12 +88,11 @@ class DustParticle(Particle):
                 self.relpos.append(knot + vers)
 
 
-
-
-
+        # Create circles at previously generated positions
         for pos in self.relpos:
             self.circles.append(self.Circle(pos, self.circleRad()))
 
+        # Calculate distance from center to furthest point
         max = 0
         for cir in self.circles:
             if (np.linalg.norm(cir.pos) + cir.rad) > max:
@@ -86,15 +102,31 @@ class DustParticle(Particle):
 
 
     class Circle:
+        """
+        Helper class abstracts a circle at position pos and radius rad
+        """
         def __init__(self, pos, rad):
+            """
+            Initializes new Circle
+            :param pos: Position
+            :param rad: Radius
+            """
             self.pos = pos
             self.rad = rad
 
 
     def poss(self):
+        """
+        Possibility to add another fractal root
+        :return:
+        """
         return (1/self.arms) * self.size / len(self.relpos)
 
     def circleRad(self):
+        """
+        Generate random circle radius
+        :return:
+        """
         if self.circle_min >= self.cirlce_max:
             return self.circle_min
         try:
@@ -103,6 +135,10 @@ class DustParticle(Particle):
             print("Error for radnind({}, {})".format(self.circle_min, self.cirlce_max))
 
     def armLength(self):
+        """
+        Generate random armlength
+        :return:
+        """
         if self.arm_min >= self.arm_max:
             return self.arm_min
         try:
@@ -111,13 +147,24 @@ class DustParticle(Particle):
             print("Error for radnind({}, {})".format(self.arm_min, self.arm_max))
 
     def angle(self):
+        """
+        Generate random angle
+        :return:
+        """
         return 2 * np.pi * random.random()
 
     def efficient_Matrix_turned(self):
+        """
+        Used for visualization. efficient matrix does not have to be turned as all angles are random
+        :return:
+        """
         return self.efficient_Matrix()
 
     def efficient_Matrix(self):
-
+        """
+        Visualizes the Dust particle as a matrix
+        :return:
+        """
         eff_matrix = np.zeros((2 * self.er, 2 * self.er))
         for i in range(-1 * self.er, 1 * self.er):
             for j in range(-1 * self.er, 1 * self.er):
@@ -132,6 +179,12 @@ class DustParticle(Particle):
         return eff_matrix, self.x, self.y
 
     def visualize_pixel(self, x, y):
+        """
+        Visualizes pixel at position (x, y) from center, see also Particle.visualize_pixel()
+        :param x: position
+        :param y: position
+        :return:
+        """
         ret = 0
         for c in self.circles:
             if np.linalg.norm(c.pos - np.array([x, y])) < c.rad:
@@ -141,6 +194,10 @@ class DustParticle(Particle):
 
     @staticmethod
     def test():
+        """
+        Testing method, measures size in dependence on other parameters
+        :return:
+        """
         print("Abhängigkeit von Size:")
         divs = []
         abws = []
